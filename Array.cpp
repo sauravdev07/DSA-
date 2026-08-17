242f67b6aadec1cbb1b822c418e65d54bc68cdac
saurav kumar <sauravk4119@gmail.com>
2026-08-17 23:37:49 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -12,3 +12,46 @@ int main(){
         cout<<i<<" ";
     }
 }
+// take the value from user and find the sum of all the element of an array
+#include<iostream>
+using namespace std;
+int main(){
+    int sum[7];
+    int add=0;
+    for(int i =0;i<7;i++){
+        cout<<"Enter the value: "<<endl;
+        cin>>sum[i];
+    }
+    for(int i=0;i<7;i++){
+        add += sum[i];
+
+    }
+    cout<<"Sum of all the element of array is "<<add;
+}
+// take the value from the user and print the second largest element of the array 
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int largest[8];
+    int max=INT_MIN;
+   
+    for(int i=0;i<8;i++){
+        cout<<"Enter the element: ";
+        cin>>largest[i];
+    }
+    for(int i=0; i<8;i++){
+        if(largest[i]>max){
+            max=largest[i];
+        }
+    }
+    int secmax=INT_MIN;
+    for(int i =0;i<8;i++){
+        if( largest[i]!=max  && secmax<largest[i]){
+            secmax =largest[i];
+           
+        }
+        
+    }
+     cout<<secmax;
+}
