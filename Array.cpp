f17470c7cc5bdbdb5639aa34cdceafb18954a7f1
saurav kumar <sauravk4119@gmail.com>
2026-09-03 13:18:28 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -454,3 +454,18 @@ int main(){
     }
 }
 
+// 19. Given an integer array nums, rotate the array to the right  by one.
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int temp=arr[n-1];
+    for(int i =n-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=temp;
+    for(int i =0;i<n;i++){
+        cout<<arr[i];
+    }
+}
