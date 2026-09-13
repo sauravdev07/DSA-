a4b9d5212a251f1634fa1139c09bfbc09ffbf807
saurav kumar <sauravk4119@gmail.com>
2026-09-13 15:28:57 +0530

    Update Linkedlist.cpp

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -48,4 +48,38 @@ int main(){
     (a.next)->val=100;
     cout<<(((a.next)->next)->next)->val; // acessing the value of d;
     // cout<<(a.next)->val;
+} 
+
+// print using While Loops
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+int main(){
+    Node a(20);
+    Node b(30);
+    Node c(40);
+    Node d(50);
+    a.next=&b;
+    b.next=&c;
+    c.next=&d;
+    cout<<(a.next)->val;
+    cout<<endl;
+    (a.next)->val=100;
+    cout<<(((a.next)->next)->next)->val;
+    cout<<endl;
+    Node temp=a;
+    while(1){      // using while loops
+        cout<<temp.val<<" ";
+        if(temp.next==NULL) break;  // break the loop when equal to null;
+        temp =*(temp.next);
+    }
+    // cout<<(a.next)->val;
 }
